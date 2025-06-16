95e70c5c49995029479b4e6e025aacb58bca93fe
yewon97 <dpdnjs9936@gmail.com>
2025-06-16 21:32:09 +0900

    build 병렬처리

--- package/themes/build.js
+++ package/themes/build.js
@@ -1,23 +1,26 @@
 import esbuild from "esbuild";
 
-esbuild.build({
-  entryPoints: ["src/index.js"],
-  bundle: true,
-  minify: true,
-  sourcemap: true,
-  outdir: "dist",
-  format: "esm",
-});
-
-// commonjs es build
-esbuild.build({
-  entryPoints: ["src/index.js"],
-  bundle: true,
-  minify: true,
-  sourcemap: true,
-  outdir: "dist",
-  format: "cjs",
-  outExtension: {
-    ".js": ".cjs",
-  },
+Promise.all([
+  esbuild.build({
+    entryPoints: ["src/index.js"],
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    outdir: "dist",
+    format: "esm",
+  }),
+  esbuild.build({
+    entryPoints: ["src/index.js"],
+    bundle: true,
+    minify: true,
+    sourcemap: true,
+    outdir: "dist",
+    format: "cjs",
+    outExtension: {
+      ".js": ".cjs",
+    },
+  }),
+]).catch(() => {
+  console.log("Build failed");
+  process.exit(1);
 });
