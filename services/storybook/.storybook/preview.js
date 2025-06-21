8d3484da665cab50830ea06887cfc933cf93b1d0
yewon97 <dpdnjs9936@gmail.com>
2025-06-21 23:41:18 +0900

    root package.json에 watch 모드 명령어 추가

--- services/storybook/.storybook/preview.js
+++ services/storybook/.storybook/preview.js
@@ -1,10 +1,12 @@
+import "@fastcampus/themes/themes.css";
+
 /** @type { import('@storybook/react-vite').Preview } */
 const preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
