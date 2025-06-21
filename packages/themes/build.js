11b67fd426821260ce0dba6cfcc9ef18c9be7a86
yewon97 <dpdnjs9936@gmail.com>
2025-06-21 22:17:56 +0900

    esbuild 공통 생성

--- packages/themes/build.js
+++ packages/themes/build.js
@@ -1,41 +1,6 @@
-import esbuild from "esbuild";
+import run from "@fastcampus/esbuild-config";
 import pkg from "./package.json" with { type: "json" };
 
-const dev = process.argv.includes("--dev");
-const minify = !dev;
-
-const watch = process.argv.includes("--watch");
-
-const external = Object.keys({
-  ...pkg.dependencies,
-  ...pkg.peerDependencies,
+run({
+  pkg,
 });
-
-const baseConfig = {
-  entryPoints: ["src/index.ts"],
-  bundle: true,
-  minify,
-  sourcemap: true,
-  outdir: "dist",
-  target: "es2019",
-  watch,
-  external,
-};
-
-Promise.all([
-  esbuild.build({
-    ...baseConfig,
-    format: "esm",
-  }),
-  esbuild.build({
-    ...baseConfig,
-    format: "cjs",
-    outExtension: {
-      ".js": ".cjs",
-    },
-  }),
-]).catch(() => {
-  console.log("Build failed");
-  process.exit(1);
-});
-
